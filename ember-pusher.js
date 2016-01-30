0fa809db6887d94c1f10e8b111ee9f47411578fb
marco de moja <marco@marcos-iMac.homenet.telecomitalia.it>
2016-01-31 00:09:41 +0100

    module compatibility upgrade for ember 2.2

--- ember-pusher.js
+++ ember-pusher.js
@@ -267,7 +267,7 @@ define("ember-pusher/controller",
 
     __exports__.Controller = Controller;
   });
-define("ember-pusher/initializer",
+define("ember-pusher/instance-initializer",
   ["ember-pusher/controller","exports"],
   function(__dependency1__, __exports__) {
     "use strict";
@@ -285,22 +285,22 @@ define("ember-pusher/initializer",
       Ember.Application.initializer({
         name: "pusherConnected",
 
-        initialize: function(container, application) {
+        initialize: function(InstanceApp) {
           var pusherController, options, pusher, dict;
 
           dict = 'pusher:main';
-          container.register(dict, Controller);
+          InstanceApp.register(dict, Controller);
 
-          pusherController = container.lookup(dict);
-          options = application.PUSHER_OPTS;
+          pusherController = InstanceApp.lookup(dict);
+          options = InstanceApp.PUSHER_OPTS;
 
           Ember.assert("You need to provide PUSHER_OPTS on your application", options);
           Ember.assert("You need to include the pusher libraries", typeof Pusher !== 'undefined');
           pusher = new Pusher(options.key, options.connection);
           pusherController.didCreatePusher(pusher);
 
-          application.inject('controller', 'pusher', dict);
-          application.inject('route', 'pusher', dict);
+          InstanceApp.inject('controller', 'pusher', dict);
+          InstanceApp.inject('route', 'pusher', dict);
         }
       });
 
